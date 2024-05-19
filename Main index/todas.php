e4d9d59a69a3fa6017145700ce6872573bd07041
Unchajusmada <carlosluisbruzualroa@gmail.com>
2024-05-19 12:34:36 -0400

    Template para la navbar en todas las paginas

--- Main index/todas.php
+++ Main index/todas.php
@@ -43,77 +43,9 @@ include '../Auth/leer_bbdd.php'
     </div>
   </div>
   <!-- Topbar End -->
+
   <!-- Navbar Start -->
-  <div class="contenedor-navbar">
-    <div class="row px-xl-5">
-      <div class="col-lg-12">
-        <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
-          <a href="" class="text-decoration-none d-block d-lg-none">
-            <h1 class="m-0 display-5 font-weight-semi-bold">
-              <span class="text-primary font-weight-bold border pl-3">U</span>NEFA
-            </h1>
-          </a>
-          <div class="col-lg-6 col-6 text-left d-lg-none d-block">
-            <form action="busqueda.php" method="POST">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="¿Qué deseas encontrar?" name="busquedaTEG" required />
-                <div class="input-group-append">
-                  <span class="input-group-text bg-transparent text-primary">
-                    <button type="submit" class="button-10"><i class="fa fa-search"></i></button>
-                  </span>
-                </div>
-              </div>
-            </form>
-          </div>
-          <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-            <div class="navbar-nav mr-auto py-0">
-              <a href="index.php" class="nav-item nav-link pl-4 pl-md-4 pr-md-4">Inicio</a>
-              <a href="todas.php" class="nav-item nav-link pl-4 pl-md-4 pr-md-4">Todas</a>
-              <div class="nav-item dropdown d-block d-lg-none pl-4 pl-md-4">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Categorias</a>
-                <div class="dropdown-menu rounded-0 m-0">
-                  <a href="autor.php" class="dropdown-item">Autor</a>
-                  <a href="publicacion.php" class="dropdown-item">Año</a>
-                  <a href="titulo.php" class="dropdown-item">Titulo</a>
-                  <a href="tutor.php" class="dropdown-item">Tutor</a>
-                </div>
-              </div>
-              <div class="nav-item dropdown d-block d-lg-none pl-4 pl-md-4">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Carreras</a>
-                <div class="dropdown-menu rounded-0 m-0">
-                  <a href="carreras.php?carrera=Aeronautica" class="dropdown-item">Ing. Aeronautica</a>
-                  <a href="carreras.php?carrera=Civil" class="dropdown-item">Ing. Civil</a>
-                  <a href="carreras.php?carrera=Electrica" class="dropdown-item">Ing. Electrica</a>
-                  <a href="carreras.php?carrera=Electronica" class="dropdown-item">Ing. Electronica</a>
-                  <a href="carreras.php?carrera=Sistemas" class="dropdown-item">Ing. de <br>Sistemas</a>
-                  <a href="carreras.php?carrera=Telecom" class="dropdown-item">Ing. de <br>Telecomunicaciones</a>
-                </div>
-              </div>
-              <a href="contact.html" class="nav-item nav-link pl-4 pl-md-4">Contacto</a>
-            </div>
-            <div class="col-lg-6 col-6 text-left d-lg-block d-none">
-              <form action="busqueda.php" method="POST">
-                <div class="input-group">
-                  <input type="text" class="form-control" placeholder="¿Que deseas encontrar?" name="busquedaTEG" required />
-                  <div class="input-group-append">
-                    <span class="input-group-text bg-transparent text-primary">
-                      <button type="submit" class="button-10">Buscar &nbsp;<i class="fa fa-search"></i></button>
-                    </span>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="navbar-nav ml-auto py-0">
-              <a href="../Admin/Paginas/login.html" class="nav-item nav-link pl-4 pl-md-4">Cargar</a>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </div>
-  </div>
+  <?php include 'template/navbar.php'; ?>
   <!-- Navbar End -->
 
   <main>
